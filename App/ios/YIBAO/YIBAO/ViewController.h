44a76938b80124ddeee2db919b038373cffb43af
Tipharodr <2075151635@qq.com>
2016-01-09 14:44:40 +0800

    素材

---
+++ App/ios/YIBAO/YIBAO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YIBAO
+//
+//  Created by menghaizhang on 16/1/5.
+//  Copyright © 2016年 ZJU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ App/ios/YIBAO/YIBAO/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  YIBAO
+//
+//  Created by menghaizhang on 16/1/5.
+//  Copyright © 2016年 ZJU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
