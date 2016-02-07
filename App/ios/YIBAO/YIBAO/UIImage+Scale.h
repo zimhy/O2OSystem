21b6c826e06b31cb12fdeb3d4771b827be7d8add
zimhy <466916676@qq.com>
2016-02-07 17:28:41 +0800

    ios

---
+++ App/ios/YIBAO/YIBAO/UIImage+Scale.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Scale.h
+//  YIBAO
+//
+//  Created by menghaizhang on 16/2/4.
+//  Copyright © 2016年 ZJU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIImage_Scale : NSObject
+
+@end
---
+++ App/ios/YIBAO/YIBAO/UIImage+Scale.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+Scale.m
+//  YIBAO
+//
+//  Created by menghaizhang on 16/2/4.
+//  Copyright © 2016年 ZJU. All rights reserved.
+//
+
+#import "UIImage+Scale.h"
+
+@implementation UIImage_Scale
+
+@end
