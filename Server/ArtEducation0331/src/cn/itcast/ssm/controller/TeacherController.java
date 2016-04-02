6079e219ab9fc87387b0023608437db593b551a2
zimhy <466916676@qq.com>
2016-04-02 21:51:12 +0800

    a

--- Server/ArtEducation0331/src/cn/itcast/ssm/controller/TeacherController.java
+++ Server/ArtEducation0331/src/cn/itcast/ssm/controller/TeacherController.java
@@ -1,12 +1,17 @@
 package cn.itcast.ssm.controller;
 
+import java.io.File;
+import java.util.UUID;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jdt.internal.compiler.env.IGenericField;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import cn.itcast.ssm.po.Teacher;
@@ -44,25 +49,44 @@ public class TeacherController {
 	
 	//教师注册
 	@RequestMapping(value="/registerTeacher.action",method=RequestMethod.POST)
-	public ModelAndView registerBaseInfo(Teacher teacher ,HttpServletRequest request){
-		//TODO
+	public ModelAndView registerBaseInfo(Teacher teacher ,MultipartFile headPortraits , //接受商品的图片
+            MultipartFile idPic,MultipartFile diplomaPic,MultipartFile masterDiplomaPic){
+		
 		ModelAndView modelAndView=new ModelAndView();
-		String stepStr = request.getParameter("step") ;
-		if(!stepStr.isEmpty()&&stepStr.matches("\\d+")) 
-		{
-			int step = Integer.parseInt(stepStr);
-			switch (step) {
-			case value:
-				
-				break;
-
-			default:
-				break;
-			}
-		}else {
+		if (headPortraits !=null) {
+			String headPortraitsStr = savePic(headPortraits) ;
+		}
+		return modelAndView ;
+	}
+	
+	private String savePic(MultipartFile file )
+	{
+		try {
 			
+			String originalFilename=file.getOriginalFilename();
+
+			if(file!=null&&originalFilename!=null&&originalFilename.length()>0){
+//				存储图片的物理路径
+				String pic_path="imageUpload/";
+//				新的图片名称
+				String newFileName=UUID.randomUUID()+originalFilename.substring(originalFilename.lastIndexOf('.'));
+//				新图片
+				File newFile=new File(pic_path+newFileName);
+//				将内存中的数据写入磁盘
+				file.transferTo(newFile);
+				return newFileName ;
+//				将新的图片名称写进itemsCustom中
+			}
+				
+			return "" ;
+			
+			
+			
+			
+		} catch (Exception e) {
+			return "";
 		}
 		
-		return modelAndView ;
+		
 	}
 }
